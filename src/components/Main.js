454d7d2c77271ffdedf0937dd50f2cbfaa3878e4
Alex Shaw <alex@threesixtytwo.co>
2019-03-10 23:59:15 +0000

    cleaning up the mess I made

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import "cloudinary-video-player/dist/cld-video-player.js"
-import "cloudinary-video-player/dist/cld-video-player.min.css"
 import pic03 from '../images/ani.gif'
 
 class Main extends React.Component {
@@ -13,19 +11,18 @@ class Main extends React.Component {
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
-          <span className="image main"></span>
+          <span className="image main"><img src={pic03} alt="" /></span>
           <p>At a point three hundred and sixty two metres above sea level, where a footpath crosses a long railway line is a gate. 
             Behind the gate is a private track, for 9 miles the track runs along a lake before it curves 
              and winds away from the water towards a range of remote mountains. In this wilderness, amoungst the swirling, unpredictable, conditions
              lies the beginning of our adventure.</p>
             <p>At three sixty two we thrive on finding customer pain and connecting simple engineering. </p><p>Aruthur C Clarke's third law reads <quote>"Any sufficiently advanced technology is indistinguishable from magic."</quote> our goal is to take the magic out of technology and put it into your business.</p>
             <p>We combine technology expertise with years of creating digital products, propositions that delight millions of
-              customers every single day, products that we are proud of. In an evolving landscape subject to unpredictable change, 
-              we've learnt adaptability, mindset and continual learning are keys to success and that the right people, 
-              with the right environment can create outstanding results.  We don't always travel the route we planed, we listen to customers, 
-            we compliment technology and we evolve our goals to suit conditions. We've gotten pretty good at creating a culture that succeeds.</p>
+              customers every single day, products that we are proud of. 
+              We've learnt adaptability, mindset and continual learning coupled with the right people, 
+              given the right environment can create outstanding results.  We've not always travelled the route we planed, because we've listened to customers and we've evolved our goals to suit conditions. We've gotten pretty good at creating a culture that succeeds.</p>
               <p>When facing your venture in an evolving digital landscape, anyone can succeed but it really helps to have a guide.</p>
-        
+              {close}
 </article>
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
