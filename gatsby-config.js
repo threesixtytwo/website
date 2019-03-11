3a8aa982f6bcf969a74359300e7f1347b38adf6f
Alex Shaw <alex@threesixtytwo.co>
2019-03-11 13:28:29 +0000

    header colour reverted

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#616161',
+        theme_color: '#616161',
         display: 'minimal-ui',
         icon: 'src/images/362portblack.png', // This path is relative to the root of the site.
       },
