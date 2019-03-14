a43ab582985ec8aebe43d19bd82d44b0e58e3347
Alex Shaw <alex@threesixtytwo.co>
2019-03-14 13:06:18 +0000

    content update

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,16 +12,17 @@ class Main extends React.Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
+          
+          
           <p>At a point three hundred and sixty two metres above sea level, where a footpath crosses a long railway line is a gate. 
-            Behind the gate is a private track, for 9 miles the track runs along a lake before it curves 
-             and winds away from the water towards a range of remote mountains. In this wilderness, amoungst the swirling, unpredictable, conditions
-             lies the beginning of our adventure.</p>
-            <p>At three sixty two we thrive on finding customer pain and connecting it to technology. </p><p>Aruthur C Clarke's third law reads <quote>"Any sufficiently advanced technology is indistinguishable from magic."</quote> our goal is to take the magic out of technology and put it into your business.</p>
-            <p>We combine technology expertise with years of creating digital products, propositions that delight millions of
-              customers every single day, products that we are proud of. 
-              We've learnt adaptability, mindset and continual learning coupled with the right people 
-              given the right environment can create outstanding results.  We've not always travelled the route we planed, because we've listened to customers, we've evolved our goals to suit conditions. We've gotten pretty good at creating a culture that succeeds.</p>
-              <p>When facing your venture in an evolving digital landscape, anyone can succeed but it really helps to have a guide.</p>
+            Beyond the gate is a track, for 9 miles the track runs along a lake before it curves 
+             away from the water towards a range of remote mountains. In this wilderness, amongst the swirling, unpredictable, conditions
+             lies the beginning of adventure stories, a place where experience helps but no two days are the same, where a life of learning can only ever change the odds. </p>
+            <p>At three sixty two we operate in a wilderness between innovation and technology.</p>
+               <p>Arthur C Clarke's third law reads <quote>"Any sufficiently advanced technology is indistinguishable from magic."</quote> Its amazing how many people say that about even their smart phone. We'd love to demystify the magic out of technology and put it into your business.</p>
+            <p>We've learnt adaptability, mindset and continual learning coupled with the right people 
+              given the right environment creates outstanding measurable, repeatable results.  We've not always travelled the route we planed, because listening to customers teaches us new things everyday. We've evolved our goals to suit conditions and we've gotten pretty good at creating a culture that succeeds.</p>
+              <p>When facing your adventure in an evolving digital landscape, anyone can succeed but, when going through your gate, it really helps to have a guide.</p>
               {close}
 </article>
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
